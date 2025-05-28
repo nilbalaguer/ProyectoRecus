639a49ca95f7353c91d31f1dee1ca3719c74cf96
nilbalaguer <nilbalaguerfp@ibf.cat>
2025-05-28 17:59:19 +0200

    Los grupos de amigos funcionan

--- Proyecto2Recus/app/Models/FriendGroup.php
+++ Proyecto2Recus/app/Models/FriendGroup.php
@@ -19,7 +19,7 @@ class FriendGroup extends Model
     // Relación N:M con usuarios (miembros del grupo)
     public function friends()
     {
-        return $this->belongsToMany(User::class, 'friend_groups_friends', 'friend_group_id', 'friends_id');
+        return $this->belongsToMany(User::class, 'friend_groups_friends', 'friend_group_id', 'user_id');
     }
 
     // Relación 1:N con el usuario que creó el grupo
